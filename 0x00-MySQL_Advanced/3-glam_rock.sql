a8dea34c9b1fb5dc8877e7b74c0fb38e0f6ea3d3
kirlos basta <kirlosbasta@gmail.com>
2024-07-12 01:51:22 +0300

    Add 2-fans.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 --  lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, CASE WHEN split IS NOT NULL THEN split - formed ELSE YEAR(CURDATE()) - formed END AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
+SELECT band_name, CASE WHEN split IS NOT NULL THEN split - formed ELSE 2020 - formed END AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
