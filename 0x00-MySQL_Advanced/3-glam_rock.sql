3cd3ec634a565d0405025d1b7de9d48cb7aec16c
kirlos basta <kirlosbasta@gmail.com>
2024-07-11 23:53:58 +0300

    Add 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+--  lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, COALESCE(split, YEAR(CURDATE())) - formed  AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
